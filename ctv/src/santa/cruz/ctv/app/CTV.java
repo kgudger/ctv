be93f895aba6e060662fcbf6f1051f769dce53e2
kgudger@gmail.com <kgudger@gmail.com@e260ce5d-b39b-c1ab-a4c6-2a7de7e9bd62>
2012-03-16 17:04:19 +0000

    added new view

--- ctv/src/santa/cruz/ctv/app/CTV.java
+++ ctv/src/santa/cruz/ctv/app/CTV.java
@@ -1,28 +1,43 @@
 package santa.cruz.ctv.app;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class CTV extends Activity {
-	TextView text;
-	URL one;
+public class CTV extends Activity 
+	implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        try {
-			one = new URL("http://www.google.com");
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        text = (TextView) findViewById(R.id.simpleText);
-        text.setText(one.getHost());
+        View ctv1Button = findViewById(R.id.ctv1Button);
+        ctv1Button.setOnClickListener(this);
     }
     
+    public void onClick(View v) {
+	
+	   if(v.getId() == R.id.ctv1Button)
+	   {
+		   //
+		   AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		   builder.setMessage("Are you sure?")
+		          .setCancelable(false)
+		          .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+		              public void onClick(DialogInterface dialog, int id) {
+		            	  dialog.cancel();
+		              }
+		          })
+		          .setNegativeButton("No", new DialogInterface.OnClickListener() {
+		              public void onClick(DialogInterface dialog, int id) {
+		                   dialog.cancel();
+		              }
+		          });
+		   AlertDialog alert = builder.create(); 
+		   alert.show();
+	   }
+	}
 }
