2dadc56b94409034a535a8eaec08b6cc362f3d91
kgudger@gmail.com <kgudger@gmail.com@e260ce5d-b39b-c1ab-a4c6-2a7de7e9bd62>
2012-03-16 17:06:44 +0000

    added new view

---
+++ ctv/src/santa/cruz/ctv/app/CTV1Stream.java
@@ -0,0 +1,16 @@
+package santa.cruz.ctv.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CTV1Stream extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {          
+
+        super.onCreate(savedInstanceState);    
+        setContentView(R.layout.ctv1stream);
+
+
+    }
+}
